e18c0f76e301994d43039420cf4994ffe9faa0ed
kindit <deltaxteam78@gmail.com>
2025-05-12 04:56:25 +0500

    AdditionalService to db

---
+++ src/main/java/org/kindit/hotel/data/additionalService/AdditionalServiceRepository.java
@@ -0,0 +1,5 @@
+package org.kindit.hotel.data.additionalService;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AdditionalServiceRepository extends JpaRepository<AdditionalService, Integer> {}
