4dfae93f9c857ddfc200d60d72cf2375a3948ac7
kindit <deltaxteam78@gmail.com>
2025-05-11 09:24:40 +0500

    RoomService

---
+++ src/main/java/org/kindit/hotel/endpoits/room/RoomService.java
@@ -0,0 +1,134 @@
+package org.kindit.hotel.endpoits.room;
+
+import jakarta.persistence.EntityNotFoundException;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.kindit.hotel.data.room.Room;
+import org.kindit.hotel.endpoits.ServiceController;
+import org.kindit.hotel.endpoits.room.request.RoomRequest;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Getter
+@Service
+@RequiredArgsConstructor
+public class RoomService extends ServiceController {
+
+    private final String uploadDir = "uploads/rooms/";
+
+    public List<Room> getAllRooms() {
+        return repository.getRoomRepository().findAll();
+    }
+
+    public Optional<Room> getRoomById(Integer id) {
+        return repository.getRoomRepository().findById(id);
+    }
+
+    public List<Room> getAllAvailableRoom() {
+        return repository.getRoomRepository().findByIsAvailableTrue();
+    }
+
+    public Room createRoom(RoomRequest request) {
+        String imageName = "";
+
+        if (!request.getImage().isEmpty()) {
+            imageName = UUID.randomUUID() + "_" + request.getImage().getOriginalFilename();
+
+            Path imagePath = Path.of(uploadDir + imageName);
+            try {
+                Files.copy(request.getImage().getInputStream(), imagePath.toAbsolutePath(), StandardCopyOption.REPLACE_EXISTING);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        Room room = Room.builder()
+                .number(request.getNumber())
+                .pricePerNight(request.getPricePerNight())
+                .capacity(request.getCapacity())
+                .description(request.getDescription())
+                .imagePath(imageName)
+                .isAvailable(request.getIsAvailable())
+                .build();
+
+        return repository.getRoomRepository().save(room);
+    }
+
+    public Optional<Room> updateAllRoom(Integer id, RoomRequest request) {
+        return repository.getRoomRepository().findById(id).map(existing -> {
+            existing.setNumber(request.getNumber());
+            existing.setPricePerNight(request.getPricePerNight());
+            existing.setDescription(request.getDescription());
+            existing.setAvailable(request.getIsAvailable());
+
+            if (request.getImage() != null && !request.getImage().isEmpty()) {
+                String imageName = UUID.randomUUID() + "_" + request.getImage().getOriginalFilename();
+                Path imagePath = Path.of(uploadDir + imageName);
+                try {
+                    Files.copy(request.getImage().getInputStream(), imagePath.toAbsolutePath(), StandardCopyOption.REPLACE_EXISTING);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+                existing.setImagePath(String.valueOf(imagePath));
+            }
+
+            return repository.getRoomRepository().save(existing);
+        });
+    }
+
+    public Optional<Room> updateRoom(Integer id, RoomRequest request) {
+        return repository.getRoomRepository().findById(id).map(existing -> {
+            if (request.getNumber() != null)
+                existing.setNumber(request.getNumber());
+
+            if (request.getPricePerNight() != null)
+                existing.setPricePerNight(request.getPricePerNight());
+
+            if (request.getDescription() != null)
+                existing.setDescription(request.getDescription());
+
+            if (request.getIsAvailable() != null)
+                existing.setAvailable(request.getIsAvailable());
+
+            if (request.getImage() != null && !request.getImage().isEmpty()) {
+                String imageName = UUID.randomUUID() + "_" + request.getImage().getOriginalFilename();
+                Path imagePath = Path.of(uploadDir + imageName);
+                try {
+                    Files.copy(request.getImage().getInputStream(), imagePath.toAbsolutePath(), StandardCopyOption.REPLACE_EXISTING);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+                existing.setImagePath(String.valueOf(imagePath));
+            }
+
+            return repository.getRoomRepository().save(existing);
+        });
+    }
+
+    public boolean deleteRoom(Integer id) {
+        Room room = repository.getRoomRepository()
+                .findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Room not found"));
+
+        if (room.getImagePath() != null) {
+            Path imagePath = Path.of(room.getImagePath()).toAbsolutePath();
+            try {
+                Files.deleteIfExists(imagePath);
+            } catch (IOException e) {
+                throw new RuntimeException("Не удалось удалить изображение: " + imagePath, e);
+            }
+        }
+
+        repository.getRoomRepository().deleteById(id);
+
+        return repository.getRoomRepository().findById(id).isEmpty();
+    }
+
+}
