28142267379a57a610897009f714cf4c37dc85aa
kindit <deltaxteam78@gmail.com>
2025-05-12 05:00:32 +0500

    Add AdditionalService to repositories class

--- src/main/java/org/kindit/hotel/Repository.java
+++ src/main/java/org/kindit/hotel/Repository.java
@@ -2,6 +2,7 @@ package org.kindit.hotel;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import org.kindit.hotel.data.additionalService.AdditionalServiceRepository;
 import org.kindit.hotel.data.room.RoomRepository;
 import org.kindit.hotel.data.user.UserRepository;
 import org.springframework.stereotype.Component;
@@ -10,7 +11,7 @@ import org.springframework.stereotype.Component;
 @AllArgsConstructor
 @Getter
 public class Repository {
-
     private final UserRepository userRepository;
     private final RoomRepository roomRepository;
+    private final AdditionalServiceRepository additionalServiceRepository;
 }
