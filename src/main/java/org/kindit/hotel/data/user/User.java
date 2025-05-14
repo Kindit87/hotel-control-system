4301fd3ba2523694aa1196f44a60390160722835
kindit <deltaxteam78@gmail.com>
2025-05-14 07:18:52 +0500

    The user now has an image

--- src/main/java/org/kindit/hotel/data/user/User.java
+++ src/main/java/org/kindit/hotel/data/user/User.java
@@ -27,6 +27,7 @@ public class User implements UserDetails {
     @Column(unique = true, nullable = false)
     private String email;
 
+    private String image;
     private String password;
 
     @Enumerated(EnumType.STRING)
--- src/main/java/org/kindit/hotel/endpoits/user/request/UserRequest.java
+++ src/main/java/org/kindit/hotel/endpoits/user/request/UserRequest.java
@@ -2,6 +2,7 @@ package org.kindit.hotel.endpoits.user.request;
 
 import lombok.Data;
 import org.kindit.hotel.data.user.Role;
+import org.springframework.web.multipart.MultipartFile;
 
 @Data
 public class UserRequest {
@@ -10,4 +11,5 @@ public class UserRequest {
     private String email;
     private String password;
     private Role role;
+    private MultipartFile image;
 }
