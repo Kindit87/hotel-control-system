fe8c4149972d77384c62f863b829bc9845f1891f
kindit <deltaxteam78@gmail.com>
2025-05-14 07:24:27 +0500

    Getting the user who accessed the endpoint is placed in a separate ancestor method

--- src/main/java/org/kindit/hotel/endpoits/ServiceController.java
+++ src/main/java/org/kindit/hotel/endpoits/ServiceController.java
@@ -1,9 +1,35 @@
 package org.kindit.hotel.endpoits;
 
 import org.kindit.hotel.Repository;
+import org.kindit.hotel.data.user.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
+import java.util.UUID;
 
 public abstract class ServiceController {
     @Autowired
     protected Repository repository;
+
+    protected final User getAuthentifactedUser() {
+        return  (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+    }
+
+    protected final String saveImage(Path destinationFolder, MultipartFile image) {
+        String imageName = UUID.randomUUID() + "_" + image.getOriginalFilename();
+
+        Path imagePath = Path.of(destinationFolder + imageName);
+        try {
+            Files.copy(image.getInputStream(), imagePath.toAbsolutePath(), StandardCopyOption.REPLACE_EXISTING);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        return imageName;
+    }
 }
